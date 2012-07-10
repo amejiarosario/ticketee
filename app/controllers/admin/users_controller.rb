9c01abbd25e0e09380a666f631b62ba9ba655248
Adrian Mejia <Adrian.Mejia@adtran.com>
2012-07-10 17:19:14 -0400

    edit and show users added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_filter :authorize_admin!
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
 
   def index
   	@users = User.all(order: 'email')
@@ -18,5 +19,31 @@ class Admin::UsersController < ApplicationController
 	    render action: "new"
 	  end
 	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+	  if params[:user][:password].blank?
+	    params[:user].delete(:password)
+	    params[:user].delete(:password_confirmation)
+	  end
+
+		@user.skip_confirmation!
+		if @user.update_attributes(params[:user]) 
+			redirect_to admin_users_path, notice: "User has been updated."
+		else
+			flash.now[:alert] = "User has not been updated."
+			render "edit"
+		end
+	end
+
+	private
+		def find_user
+			@user = User.find(params[:id])
+		end
 
 end
